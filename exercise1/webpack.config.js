e5700e701158c4a13fe6134d2aad9ed8002b65b0
白夜 <18556134754@163.com>
2022-09-25 21:58:24 +0800

    some update

--- exercise1/webpack.config.js
+++ exercise1/webpack.config.js
@@ -1,6 +1,19 @@
 const path = require('path');
 
 module.exports = {
+    // 文件监听是在发现源码发生变化时，自动重新构建出新的输出文件
+    // 开启监听模式有两种方法：1.启动 webpack 命令时，带上 --watch 参数；2.配置 webpack.config.js 中设置 watch:true
+    // 唯一缺陷：新的构建输出必须手动重新加载（重新刷新浏览器等）
+    watch: true,
+    // 文件监听的原理分析：轮询判断文件的最后编辑时间是否变化（某个文件发生了变化，并不会立刻告知监听者，而是先缓存起来，等 aggregateTimeout 后统一告知）
+    watchOptions: {
+        // 默认为空，不监听的文件或者文件夹，支持正则匹配
+        ignored: /node_modules/,
+        // 监听到变化发生后会等 300 ms 后再去重新构建，默认 300 ms
+        aggregateTimeout: 300,
+        // 判断文件是否发生变化是通过不停询问系统指定文件是否发生变化实现的，默认每秒询问 1000 次
+        poll: 1000
+    },
     entry: {
         bundle1: './src/entry1.js',
         bundle2: './src/entry2.js'
