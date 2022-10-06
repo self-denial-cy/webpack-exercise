19b680df5b4521e5a1f96ea7070689091fcfc170
白夜 <18556134754@163.com>
2022-10-06 19:19:46 +0800

    some update

--- exercise1/webpack.mpa.js
+++ exercise1/webpack.mpa.js
@@ -23,6 +23,20 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 // 原理：利用 ES6 模块的特点：1.只能作为模块顶层的语句出现；2.import 的模块名只能是字符串常量；3.import binding 是 immutable 的
 // DCE（dead code elimination）：代码不会被执行，不可到达|代码执行的结果不会被用到|代码只会影响死变量（只写不读）
 
+// Scope Hoisting
+// 现象：构建后的代码中存在大量闭包代码
+// 导致问题：1.大量函数闭包包括代码，导致体积增大（模块越多越明显）；2.运行代码时创建的函数作用域变多，内存开销变大
+// 模块转换分析：1.被 webpack 转换后的模块会带上一层包裹；2.import 会被转换成 __webpack_require__、export 会被转换成 __webpack_exports__
+// 进一步分析 webpack 的模块机制：
+// 1.打包出来的是一个 IIFE（匿名闭包）
+// 2.modules 是一个数组，每一项是一个模块初始化函数
+// 3.__webpack_require__ 用来加载模块，返回 module.exports
+// 4.通过 __webpack_require__(0) 启动（模块 0 一般是入口文件）
+// Scope Hoisting（与 tree shaking 一样都是借鉴自 rollup）
+// 原理：将所有模块的代码按照引用顺序放在一个函数作用域里，然后适当的重命名一些变量以防止变量名冲突
+// 对比：通过 Scope Hoisting 可以减少函数声明代码和内存开销
+// 使用：webpack mode 为 production 默认开启（必须是 ES6 语法，CJS 不支持）
+
 const setMPA = () => {
     const entry = {};
     const htmlWebpackPlugins = [];
