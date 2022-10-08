a3a736356c0cdebdc4edd1d12a40dc5c410c1cd9
Scorpio <18556134754@163.com>
2022-10-08 17:46:40 +0800

    some update

--- exercise1/server/index.js
+++ exercise1/server/index.js
@@ -6,9 +6,12 @@ const express = require('express');
 const {renderToString} = require('react-dom/server');
 const SSR = require('../dist/home');
 const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf-8');
+// 首屏数据（只是首屏数据而已）（tag:暂时还不清楚这个首屏数据搁哪使用）
+const data = require('./data.json');
 
 const renderMarkup = (html) => {
-    return template.replace('<!--HTML_PLACEHOLDER-->', html);
+    const json = JSON.stringify(data);
+    return template.replace('<!--HTML_PLACEHOLDER-->', html).replace('<!--INITIAL_DATA_PLACEHOLDER-->', `<script>window.__initial_data__=${json}</script>`);
 };
 
 const server = (port) => {
